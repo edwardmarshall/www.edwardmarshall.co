ac2f345a8610c6019e883ba36995360435393de4
Eric Taylor <eric@daxos.com>
2013-05-10 13:00:01 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edwardmarshall::Application.config.secret_token = '8cfbfb24ac3e0edb119b91648bfb5374abd739522028e56f6441361442b9132fb75b0f687ff1cf1817d0f1d2b3ce48b104520c99c246f07206ac71409ed96b8d'
